bc392658876a6142c2a73c5f6cc169e0a31297ba
bluvenr <bluvenr@outlook.com>
2026-06-25 12:48:41 +0800

    chore: bump version to 1.1.4

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ package version
 // It can be overridden at build time via:
 //
 //	-ldflags "-X github.com/bluvenr/hookrun/internal/version.Version=x.y.z"
-var Version = "1.1.3"
+var Version = "1.1.4"
 
 // BuildTime is the build timestamp, set via ldflags.
 var BuildTime = "unknown"
